6ef73831b3d4e50815bbac28de113f2dad2abfe8
Ismael Meza Rodríguez <47857223+MezaDios@users.noreply.github.com>
2021-02-16 16:49:13 -0600

    Inicializacion del proyecto

---
+++ chess/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Chessboard from './components/Chessboard/Chessboard';
+
+function App() {
+  return (
+    <div id="container">
+      <Chessboard />
+    </div>
+  );
+}
+
+export default App;
